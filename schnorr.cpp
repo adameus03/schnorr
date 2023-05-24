99b17d11178943f6601f58f922979812df893ecb
Amadeusz Sitnicki <amadeusz.sitnicki1@gmail.com>
2023-05-24 21:14:54 +0200

    Schnorr source/header added

---
+++ schnorr.cpp
@@ -0,0 +1,10 @@
+#include "buffer_arithmetics.h"
+
+#define P_LENGTH 140
+#define Q_LENGTH 512
+
+typedef unsigned long long ull;
+
+void generate_pqh(ull* p, ull* q, ull* h){
+
+}
---
+++ schnorr.h
@@ -0,0 +1,6 @@
+#ifndef SCHNORR_H_INCLUDED
+#define SCHNORR_H_INCLUDED
+
+void generate_pqh(ull* p, ull* q, ull* h);
+
+#endif // SCHNORR_H_INCLUDED
